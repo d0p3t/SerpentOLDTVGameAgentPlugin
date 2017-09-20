be3d22f03cc3cd03c21e2795800e55959c90e9f1
Remco Troost <remcotroost@outlook.com>
2017-09-20 16:47:44 +0200

    before reinstall

--- files/serpent_OLDTV_game_agent.py
+++ files/serpent_OLDTV_game_agent.py
@@ -1,5 +1,12 @@
+import serpent.cv
+
 from serpent.game_agent import GameAgent
 
+from serpent.machine_learning.context_classification.context_classifiers.cnn_inception_v3_context_classifier import CNNInceptionV3ContextClassifier
+
+import offshoot
+
+import time
 
 class SerpentOLDTVGameAgent(GameAgent):
 
@@ -13,7 +20,23 @@ class SerpentOLDTVGameAgent(GameAgent):
         self.analytics_client = None
 
     def setup_play(self):
-        pass
+        plugin_path = offshoot.config["file_paths"]["plugins"]
+
+        context_classifier_path = f"{plugin_path}/SerpentOLDTVGameAgentPlugin/files/ml_models/context_classifier.model"
+
+        context_classifier = CNNInceptionV3ContextClassifier(input_shape=(289, 512, 3)) # GET new input_shape size
+        context_classifier.prepare_generators()
+        context_classifier.load_classifier(context_classifier_path)
+
+        self.machine_learning_models["context_classifier"] = context_classifier
 
     def handle_play(self, game_frame):
-        pass
+        context = self.machine_learning_models["context_classifier"].predict(game_frame.frame)
+
+        if context == "main_menu":
+            print("CONTEXT: main_menu")
+            self.game.api.MainMenu.click_options()
+            time.sleep(1)
+        elif context == "options_menu":
+            print("CONTEXT: options_menu")
+            print("We are now in options!")
